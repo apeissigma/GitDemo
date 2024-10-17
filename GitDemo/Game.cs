9d5308a7d5fe894dc36a2d5c213521111f3bd075
apeissigma <apeissigma@gmail.com>
2024-10-17 15:52:05 -0500

    Created skeleton of Game class with run method

---
+++ GitDemo/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemo
+{
+    internal class Game
+    {
+
+        public void Run()
+        {
+            Console.WriteLine("Welcome to the game");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
--- GitDemo/Program.cs
+++ GitDemo/Program.cs
@@ -8,7 +8,9 @@ namespace GitDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello version control :)");
-            Console.ReadKey(true);
+
+            Game myGame = new Game();
+            myGame.Run();
         }
     }
 }
